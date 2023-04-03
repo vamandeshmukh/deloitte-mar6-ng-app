f1583f601fae856811039b14e72341ab886c088d
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-04-03 18:17:48 +0530

    angular

--- src/app/components/delete/delete.component.ts
+++ src/app/components/delete/delete.component.ts
@@ -6,8 +6,21 @@ import { Component } from '@angular/core';
 })
 export class DeleteComponent {
 
+    someData = 'Sonu';
 
+    shouldBeDisabled = false;
 
-
+    
 }
+
+
+// data binding - 
+
+// interpolation - component to template 
+// property binding - component to template 
+
+// event binding - template to component  
+
+// two -way  binding - two way 
+
 
