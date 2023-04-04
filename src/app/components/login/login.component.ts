35c85ef97bb6f7d48aff58d4ef35cb3a4c5ca502
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-04-04 12:13:06 +0530

    angular

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -18,21 +18,55 @@ export class LoginComponent {
     password: ''
   };
 
-  welcomeMessage = '';
+  isLoggedIn = false;
+
+  welcomeMessage: any = '';
 
   submitLogin = () => {
 
     this.http.get('https://jsonplaceholder.typicode.com/users/')
       .subscribe((resp: any) => {
         console.log(resp);
+        resp.forEach((element: any) => {
+          if (element.username === this.loginInput.username) {
+            this.isLoggedIn = true;
+          }
+          if (this.isLoggedIn) {
+            console.log(this.loginInput.username);
+            this.welcomeMessage = 'Logged in successfully!';
+          }
+          else {
+            this.welcomeMessage = 'Invalid Credentials!';
+          }
+          this.loginInput = { username: '', password: '' }; // clear form values
+        });
       });
-
-
-    this.welcomeMessage = 'Hi ' + this.loginInput.username + '!';
-    this.loginInput = { username: '', password: '' }; // clear form values
   };
-
 }
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-login',
+//   templateUrl: './login.component.html',
+//   styleUrls: ['./login.component.css']
+// })
+
+// export class LoginComponent {
+
+//   loginInput = {
+//     username: '',
+//     password: ''
+//   };
+
+//   welcomeMessage = '';
+
+//   submitLogin = () => {
+//     this.welcomeMessage = 'Hi ' + this.loginInput.username + '!';
+//     this.loginInput = { username: '', password: '' }; // clear form values
+//   };
+
+// }
 
 
 
