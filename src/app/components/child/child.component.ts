92d2e3ec67e1ba96e5e52f8b3bdfd3b66a8f99a2
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-04-05 12:09:42 +0530

    angular

--- src/app/components/child/child.component.ts
+++ src/app/components/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class ChildComponent {
 
-
+  @Input()
   parentDataInChild: string = '';
 
 
