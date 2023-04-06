591d6614f65798c74c757b6f95939cc0fd3c4c59
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-04-06 16:25:11 +0530

    angular

--- src/app/components/child/child.component.spec.ts
+++ src/app/components/child/child.component.spec.ts
@@ -1,11 +1,30 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { ChildComponent } from './child.component';
 
 describe('ChildComponent', () => {
+
+  // vars declaration 
+
+
+
+  it('test aitnmetic op', () => {
+    expect(10 + 10).toEqual(20);
+  });
+
+  it('test aitnmetic op', () => {
+    expect(10 + 10).not.toEqual(21);
+  });
+
+});
+
+
+describe('ChildComponent', () => {
   let component: ChildComponent;
   let fixture: ComponentFixture<ChildComponent>;
-
+  let el: DebugElement;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ChildComponent]
@@ -21,12 +40,14 @@ describe('ChildComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should render Monu', () => {
-    const fixture = TestBed.createComponent(ChildComponent);
-    fixture.detectChanges();
-
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.getElementsByTagName('p').item).toBe('Monu');
+  it('should create', () => {
+    expect(component).toBeTruthy();
   });
+
+  // it('should render Monu', () => {
+  //   const testText = el.query
+  //     (By.css('p')).nativeElement;
+  //   expect(testText.textContent).toContain('Monu');
+  // });
 
 });
